1ddd7d6dc45027e2f2e539495e8a091e70b88582
jolinohj <135192275+jolinohj@users.noreply.github.com>
2024-05-16 06:16:52 +0000

    prompt

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-function sayHello() {
-    alert("Hello, welcome to my blog!");
-}
+var guestname = prompt("Hello! What is your name?");
+var name_firstChar = guestname.slice(0,1);
+var name_restChar = guestname.slice (1,guestname.length)
+var uppercase_firstChar = name_firstChar.toUpperCase();
+var uppercase_restChar = name_restChar.toLowerCase();
+alert("Hi " + uppercase_firstChar + uppercase_restChar "!" + "Thank you for visiting my first webpage :)" + "  This a web app that aims to streamline the way people travelling in a group plan their trip - hope you find it useful!");
